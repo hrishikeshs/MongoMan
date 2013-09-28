eb0be66586ce6f0ce3e7f0aba1f6275ff1c4d756
Hrishikesh S <hrish2006@gmail.com>
2013-09-28 13:32:09 +0530

    integration with ember

---
+++ Mongoman/app/assets/javascripts/routes/mongoman_route.js
@@ -0,0 +1,5 @@
+Mongoman.Route = Ember.Route.extend({
+	enter: function() {
+		console.log("In MongoMan Route");
+	}
+})
---
+++ Mongoman/app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def index
+  end
+
+end
