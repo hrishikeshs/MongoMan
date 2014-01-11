53030a2b6398fdb79f5d7032a29d53479335347b
hrishikeshs <hrish2006@gmail.com>
2014-01-11 11:00:51 +0530

    improved the json parsing and made the display pretty.

--- Mongoman/app/assets/javascripts/helpers/parse_json_helper.js
+++ Mongoman/app/assets/javascripts/helpers/parse_json_helper.js
@@ -1,5 +1,41 @@
-Ember.Handlebars.registerBoundHelper('parseJSONString', function (text) {
-	var s = JSON.stringify(text, undefined, 2);
-	var html = "<pre>" + s + "</pre>";
-  return new Handlebars.SafeString(html);
+Ember.Handlebars.registerBoundHelper('parseJSONString', function (json) {
+
+  function prettyPrint(json) {
+
+    var type = typeof json;
+
+    switch(type) {
+      
+      case 'string' : return  "'" + json + "'" 
+      case 'number' : 
+      case 'undefined' : 
+      case 'boolean' : 
+            return json;
+      break;
+
+      default:
+        if (Array.isArray(json)) {
+          json = json.map(prettyPrint);
+          return  '[<div class="collapsible">'  + json.join(",\n") + '</div>]';
+        }
+        else {
+        //none of the above so it is the object
+          if (json !== null) {
+            var keys = Object.keys(json)
+            var htmlArray = []
+            for(var i = 0, len = keys.length; i < len; i++) {
+                var k = keys[i];
+                var html = '<br /><div class="showhide"><b>' + k + '</b>' + ':  '  + '<span style="overflow-wrap:break-word;overflow-x:visible;">'+ prettyPrint(json[k]) + '</span></div>';
+                htmlArray.push(html);
+            }
+            return '{' + '<div class="collapsible">' + htmlArray.join() + '<br />' + '</div>}';
+        }
+        return null;
+      }
+    }
+  }
+
+  var html = prettyPrint(json)
+  return new Handlebars.SafeString(html)
+
 });
