da206a79c74a82a9949e2e6e387cf07c9212589c
hrishikeshs <hrish2006@gmail.com>
2014-01-18 16:35:31 +0530

    Re-enable editing and fix bugs related to creation of documents.

--- Mongoman/app/assets/javascripts/controllers/documents_controller.js
+++ Mongoman/app/assets/javascripts/controllers/documents_controller.js
@@ -24,7 +24,14 @@ Mongoman.DocumentsController = Ember.ArrayController.extend({
 	}.observes('content.content'),
 
 
-
+  jsonifyText: function(str) {
+    var keys = str.match(/[a-zA-Z0-9_]+:\s+/g)
+    for (var i = 0, j = keys.length; i < j; i++) {
+      str = str.replace(keys[i],'"' + keys[i].split(':')[0] + '":' )
+    }
+    return str
+  },
+  
 	isValidDocument: function(payload) {
 		var result = ""
 		try {
@@ -42,16 +49,16 @@ Mongoman.DocumentsController = Ember.ArrayController.extend({
   	addDocument: function() {
   		var self = this
   		$( "#dialog-form" ).dialog({
-      resizable: false,
-      height:400,
-		  width: 500,
+      resizable: true,
+      height:450,
+		  width: 600,
 		  modal: true,
 		  buttons: {
         "Add Document" : function() {
         	var json = self.isValidDocument(self.newDocument);
         	if (json && (self.newDocument !== "")) {
-        		var url = '/documents/?' + jQuery.param({newdoc : self.newDocument}) + "&"
-        		Mongoman.PostRequest.post(url , {database_name : self.get('database_name'), collection_name: self.get('collection_name')}, 'POST')
+        		var url = '/documents/?'
+        		Mongoman.PostRequest.post(url , {database_name : self.get('database_name'), collection_name: self.get('collection_name')}, 'POST', self.jsonifyText(self.newDocument.trim()))
         		$( this ).dialog( "close" )
         	} 
         	else {
@@ -66,32 +73,7 @@ Mongoman.DocumentsController = Ember.ArrayController.extend({
 
 		},
 
-		deleteDocument: function(p) {
-			var self = this
-			$("#placeholder-confirm" ).dialog({
-		  	resizable: false,
-		  	height:250,
-		  	width: 450,
-		  	modal: true,
-		  	buttons: {
-		      Delete: function() {
-			      self.get('visibleContent').removeObject(p)
-						if (self.get('totalContent').length ) {
-							self.get('visibleContent').pushObject(self.get('totalContent').shiftObject())
-              window.scroll(0,0);
-						}
-						var url = '/documents/' + ((typeof p._id === "string") ? p._id.match(/[0-9a-f]{24}/)[0] : -1 ) + '?'
-						Mongoman.PostRequest.post(url , {document_index: JSON.stringify(p._id), database_name : self.get('database_name'), collection_name: self.get('collection_name')}, 'delete')
-						$(this).dialog("close")
-            self.set('count', self.get('count')  - 1)
-			    },
-		    	Cancel: function() {
-		     		$(this).dialog("close") 
-		      }
-			  }
-			});
-		},
-
+		
     dropCollection: function() {
       var self = this;
       $("#placeholder-confirm-drop-collection" ).dialog({
