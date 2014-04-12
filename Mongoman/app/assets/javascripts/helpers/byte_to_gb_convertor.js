2b051fbed7ce5f6fe8059371657558b2782c14c2
hrishikesh <hrishikesh.snrao@kaybus.com>
2014-04-12 15:15:38 +0530

    Humanize the sizes to make them more readable.
    Changes the byte to gb handlebars helper to convert the bytes into more
    human readable strings.

--- Mongoman/app/assets/javascripts/helpers/byte_to_gb_convertor.js
+++
@@ -1,3 +0,0 @@
-Ember.Handlebars.registerBoundHelper('convertToGB', function (number) {
- return Math.round((number / Math.pow(2,30)), 15);
-});
---
+++ Mongoman/app/assets/javascripts/helpers/humanize_helper.js
@@ -0,0 +1,21 @@
+Ember.Handlebars.registerBoundHelper('humanize', function (number) {
+  var numDigits = (number + "").length;
+
+  switch(numDigits) {
+
+    case 3:
+      return number + " bytes";
+    case 4:
+    case 5:
+    case 6:
+      return Math.floor(Math.round(number/Math.pow(2,10))) + " Kb";
+
+    case 7:
+      return Math.floor(Math.round(number/Math.pow(2,20))) + " Mb";
+
+    default:
+      return Math.floor(Math.round(number/Math.pow(2,30))) + " Gb";
+  }
+
+
+});
